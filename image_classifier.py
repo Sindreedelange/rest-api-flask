f5e22216bded95fd8701a880a9d65fb29b36be13
Sindreedelange <146169@stud.hvl.no>
2019-03-18 21:35:09 +0100

    Working inference on example image

--- image_classifier.py
+++ image_classifier.py
@@ -1,12 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from fastai.vision import load_learner, learner, open_image
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return{'about': 'Hello World'}
+        self.load_model()
+        img_fpath = os.path.join("data", "MNIST_example_3.png")
+        data = open_image(img_fpath)
+        pred_class, pred_idx, outputs = self.model.predict(data)
+        return{'pred': str(pred_class)}
+
+    def load_model(self):
+        self.model = load_learner("models", "simple_cnn.pkl")
 
 
 api.add_resource(HelloWorld, '/')
